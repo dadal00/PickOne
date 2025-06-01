da089801fc9dc9aee1bc54eb0b2b1421037b09fe
Dylan Adal <dylanadal.00@gmail.com>
2025-06-01 10:33:24 -0700

    fe: first steps for designing

---
+++ frontend/src/routes/voting/+page.svelte
@@ -0,0 +1,77 @@
+<script lang="ts">
+  import { onMount, onDestroy } from 'svelte'
+  import { websocket } from '$lib/stores/websocket'
+  import BarChart from '$lib/components/BarChart.svelte'
+  import VoteButton from '$lib/components/VoteButton.svelte'
+  import TotalHeader from '$lib/components/TotalHeader.svelte'
+  import { browser } from '$app/environment'
+  import { Color } from '$lib/models'
+
+  function setVh() {
+    document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`)
+  }
+
+  onMount(() => {
+    websocket.connect()
+    setVh()
+    window.addEventListener('resize', setVh)
+  })
+
+  onDestroy(() => {
+    if (browser) {
+      window.removeEventListener('resize', setVh)
+    }
+    websocket.disconnect()
+  })
+</script>
+
+<style>
+  :global(body, html) {
+    margin: 0;
+    touch-action: manipulation;
+  }
+
+  .page-container {
+    display: flex;
+    flex-direction: column;
+    height: calc(var(--vh, 1vh) * 100);
+    height: 100vh;
+    width: 100vw;
+    background-color: #faf4ee;
+    position: fixed;
+  }
+
+  .footer-container {
+    bottom: 0;
+    left: 0;
+    right: 0;
+    position: fixed;
+    min-height: 4rem;
+    height: calc(var(--vh, 1vh) * 17);
+    background-color: #f5f0e6;
+    border-top: 1px solid rgb(188, 185, 178);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 6vw;
+  }
+
+  .body-container {
+    overflow: hidden;
+    background-color: #faf4ee;
+    height: calc(var(--vh, 1vh) * 83);
+  }
+</style>
+
+<main class="page-container">
+  <div class="body-container">
+    <TotalHeader />
+    <BarChart />
+  </div>
+  <div class="footer-container">
+    <VoteButton color={Color.Red} />
+    <VoteButton color={Color.Blue} />
+    <VoteButton color={Color.Green} />
+    <VoteButton color={Color.Purple} />
+  </div>
+</main>
