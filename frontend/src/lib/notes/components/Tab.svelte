158af050a2ff221bbb02e4cdbbf4bc4278cfe54d
Dylan Adal <dylanadal.00@gmail.com>
2025-06-10 14:28:02 -0700

    fe: add tab

--- frontend/src/lib/notes/components/Tab.svelte
+++ frontend/src/lib/notes/components/Tab.svelte
@@ -1,8 +1,20 @@
 <script lang="ts">
-  const { name, selected } = $props<{
-    name: String
-    selected: Boolean
+  const { name, selected, add } = $props<{
+    name: string
+    selected?: boolean
+    add?: boolean
   }>()
+  const switchTab = (event: MouseEvent) => {
+    if (!selected) {
+      console.log('goto')
+    }
+  }
+  const addTab = (event: MouseEvent) => {
+    console.log('add')
+  }
+  const closeTab = (event: MouseEvent) => {
+    console.log('close')
+  }
 </script>
 
 <style>
@@ -14,7 +26,17 @@
     display: flex;
     flex-direction: row;
     align-items: center;
-    /* background-color: pink; */
+    z-index: 0;
+  }
+  .add-tab {
+    margin-left: auto;
+    width: 5vw;
+    justify-content: center;
+    color: #dfc1a3;
+    font-weight: bold;
+  }
+  .add-tab:hover {
+    opacity: 70%;
   }
   .tab-background {
     position: absolute;
@@ -29,6 +51,9 @@
     padding: 10px 20px 24px 20px;
   }
   .tab-container-unselected {
+    opacity: 40%;
+  }
+  .tab-container-unselected:hover {
     opacity: 60%;
   }
   .tab-text {
@@ -37,18 +62,26 @@
     font-size: min(1rem, 2.2vw, 4.2vh);
     white-space: nowrap;
   }
-  .tab-text:hover {
-    opacity: 10%;
+  .tab-button {
+    all: unset;
+  }
+  .tab-close-button {
+    all: unset;
+    margin-left: 15px;
+    padding: 3px;
+    border-radius: 3px;
+  }
+  .tab-close-button:hover {
+    background-color: #f8f1e2;
   }
   .x-sprite {
-    --scale: 0.075;
+    --scale: 0.08;
     background-image: url('/sprites.png');
     background-position: calc(-162px * var(--scale)) calc(-216px * var(--scale));
     background-size: calc(298px * var(--scale)) calc(357px * var(--scale));
     width: calc(126px * var(--scale));
     height: calc(140px * var(--scale));
     background-repeat: no-repeat;
-    margin-left: 15px;
   }
   @media screen and (max-width: 550px) {
     .x-sprite {
@@ -59,13 +92,25 @@
     .x-sprite {
       --scale: 0.05;
     }
+    .tab-container-unselected:hover {
+      opacity: 40%;
+    }
   }
 </style>
 
-<div class="tab-container {selected ? '' : 'tab-container-unselected'}">
-  <p class="tab-text">
-    {name}
-  </p>
-  <div class="x-sprite"></div>
-  <div class="tab-background"></div>
+<div
+  class="tab-container {!selected && !add ? 'tab-container-unselected' : ''} {add ? 'add-tab' : ''}"
+>
+  <button class="tab-button" onclick={add ? addTab : switchTab}>
+    <p class="tab-text">
+      {name}
+    </p>
+  </button>
+  {#if !add}
+    <button class="tab-close-button" aria-label="close tab button" onclick={closeTab}>
+      <div class="x-sprite"></div>
+    </button>
+  {/if}
+  <button class="tab-background" aria-label="tab-button" onclick={add ? addTab : switchTab}
+  ></button>
 </div>
--- frontend/src/lib/notes/components/TabsContainer.svelte
+++ frontend/src/lib/notes/components/TabsContainer.svelte
@@ -3,56 +3,75 @@
 </script>
 
 <style>
-  .tabs-container {
+  .note-tab-container {
     margin: calc(17vh - 35px) 0 0 calc(2.5vw + 4px);
     position: absolute;
+    width: calc(95% - 8px);
+    z-index: 0;
+    height: fit-content;
+    display: flex;
+    flex-direction: row;
+    gap: 20px;
+  }
+  .tabs-container {
     gap: 1vw;
     display: flex;
     flex-direction: row;
     overflow-x: scroll;
     -webkit-overflow-scrolling: touch;
     scrollbar-width: thin;
-    width: calc(95% - 8px);
-    height: fit-content;
-    z-index: 0;
+    width: 85%;
   }
-  /* @media screen and (max-height: 600px) {
-    .tabs-container {
+  @media screen and (max-height: 600px) {
+    .note-tab-container {
       margin-left: 10vw;
       width: 70%;
       margin-top: 11vh;
     }
   }
+  @media screen and (max-height: 520px) {
+    .note-tab-container {
+      margin-top: 11vh;
+    }
+  }
+  @media screen and (max-height: 400px) {
+    .note-tab-container {
+      margin-top: 8vh;
+    }
+  }
   @media screen and (max-height: 600px) and (max-width: 1000px) {
-    .tabs-container {
+    .note-tab-container {
       width: 67%;
     }
   }
   @media screen and (max-height: 600px) and (max-width: 700px) {
-    .tabs-container {
+    .note-tab-container {
       margin-left: 12vw;
       width: 60%;
     }
   }
   @media screen and (max-height: 600px) and (max-width: 550px) {
-    .tabs-container {
+    .note-tab-container {
       margin-left: 14vw;
       width: 55%;
     }
   }
   @media screen and (max-aspect-ratio: 0.57) and (max-width: 450px) {
-    .tabs-container {
-      margin-top: 13.5vh;
+    .note-tab-container {
+      margin-top: 13.75vh;
     }
-  } */
+  }
 </style>
 
-<div class="tabs-container">
-  <Tab name="Notes 1 ALJFLADFKJASF;LKSJLKJ" selected={true}></Tab>
-  <Tab
-    name="Notes 2 ALJFLADFKJASF;LKSJLKJALJFLADFKJASF;LKSJLKJALJFLADFKJASF;LKSJLKJ"
-    selected={false}
-  ></Tab>
-  <Tab name="Notes 3" selected={false}></Tab>
-  <Tab name="Notes 4" selected={false}></Tab>
+<div class="note-tab-container">
+  <div class="tabs-container">
+    <Tab name="Notes 1 ALJFLADFKJASF;LKSJLKJ" selected={true}></Tab>
+    <Tab
+      name="Notes 2 ALJFLADFKJASF;LKSJLKJALJFLADFKJASF;LKSJLKJALJFLADFKJASF;LKSJLKJ"
+      selected={false}
+    ></Tab>
+    <Tab name="Notes 3" selected={false}></Tab>
+    <Tab name="Notes 4" selected={false}></Tab>
+  </div>
+  <Tab name="+" add={true}></Tab>
 </div>
