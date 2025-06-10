2b7cbcdf6c527a7d7ddd6926dca5af0a5e257585
Dylan Adal <dylanadal.00@gmail.com>
2025-06-09 17:07:36 -0700

    fe: tab in progress

---
+++ frontend/src/lib/notes/components/Tab.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+  const { name, selected } = $props<{
+    name: String
+    selected: Boolean
+  }>()
+</script>
+
+<style>
+  .tab-container {
+    padding: 8px 20px 24px 20px;
+    position: relative;
+    border-top-left-radius: 11px;
+    border-top-right-radius: 11px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    /* background-color: pink; */
+  }
+  .tab-background {
+    position: absolute;
+    background-color: white;
+    border-top: 4px solid #f2dfcb;
+    border-right: 4px solid #f2dfcb;
+    border-left: 4px solid #f2dfcb;
+    inset: 0;
+    z-index: -1;
+    border-top-left-radius: 11px;
+    border-top-right-radius: 11px;
+    padding: 10px 20px 24px 20px;
+  }
+  .tab-container-unselected {
+    opacity: 60%;
+  }
+  .tab-text {
+    margin: 0;
+    padding: 0;
+    font-size: min(1rem, 2.2vw, 4.2vh);
+    white-space: nowrap;
+  }
+  .tab-text:hover {
+    opacity: 10%;
+  }
+  .x-sprite {
+    --scale: 0.075;
+    background-image: url('/sprites.png');
+    background-position: calc(-162px * var(--scale)) calc(-216px * var(--scale));
+    background-size: calc(298px * var(--scale)) calc(357px * var(--scale));
+    width: calc(126px * var(--scale));
+    height: calc(140px * var(--scale));
+    background-repeat: no-repeat;
+    margin-left: 15px;
+  }
+  @media screen and (max-width: 550px) {
+    .x-sprite {
+      --scale: 0.06;
+    }
+  }
+  @media screen and (max-aspect-ratio: 0.57) and (max-width: 450px) {
+    .x-sprite {
+      --scale: 0.05;
+    }
+  }
+</style>
+
+<div class="tab-container {selected ? '' : 'tab-container-unselected'}">
+  <p class="tab-text">
+    {name}
+  </p>
+  <div class="x-sprite"></div>
+  <div class="tab-background"></div>
+</div>
---
+++ frontend/src/lib/notes/components/TabsContainer.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+  import Tab from './Tab.svelte'
+</script>
+
+<style>
+  .tabs-container {
+    margin: calc(17vh - 35px) 0 0 calc(2.5vw + 4px);
+    position: absolute;
+    gap: 1vw;
+    display: flex;
+    flex-direction: row;
+    overflow-x: scroll;
+    -webkit-overflow-scrolling: touch;
+    scrollbar-width: thin;
+    width: calc(95% - 8px);
+    height: fit-content;
+    z-index: 0;
+  }
+  /* @media screen and (max-height: 600px) {
+    .tabs-container {
+      margin-left: 10vw;
+      width: 70%;
+      margin-top: 11vh;
+    }
+  }
+  @media screen and (max-height: 600px) and (max-width: 1000px) {
+    .tabs-container {
+      width: 67%;
+    }
+  }
+  @media screen and (max-height: 600px) and (max-width: 700px) {
+    .tabs-container {
+      margin-left: 12vw;
+      width: 60%;
+    }
+  }
+  @media screen and (max-height: 600px) and (max-width: 550px) {
+    .tabs-container {
+      margin-left: 14vw;
+      width: 55%;
+    }
+  }
+  @media screen and (max-aspect-ratio: 0.57) and (max-width: 450px) {
+    .tabs-container {
+      margin-top: 13.5vh;
+    }
+  } */
+</style>
+
+<div class="tabs-container">
+  <Tab name="Notes 1 ALJFLADFKJASF;LKSJLKJ" selected={true}></Tab>
+  <Tab
+    name="Notes 2 ALJFLADFKJASF;LKSJLKJALJFLADFKJASF;LKSJLKJALJFLADFKJASF;LKSJLKJ"
+    selected={false}
+  ></Tab>
+  <Tab name="Notes 3" selected={false}></Tab>
+  <Tab name="Notes 4" selected={false}></Tab>
+</div>
